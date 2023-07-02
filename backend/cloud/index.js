0665786fd1d9c77d66db29bf1e8fd858e61d6e2d
Lyonsmasawa <lyonsmasawa@gmail.com>
2023-07-02 07:59:47 -0700

    configur: 'cloudinary'

---
+++ backend/cloud/index.js
@@ -0,0 +1,8 @@
+import { config } from "cloudinary";
+
+config({
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_API_KEY, 
+    api_secret: process.env.CLOUD_SECRET,
+    secure: true
+});
